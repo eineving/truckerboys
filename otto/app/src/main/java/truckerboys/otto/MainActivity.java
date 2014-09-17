f05c4daf3755151ffb05e9cbc9484b0176c895ee
Martin Nilsson <martin.lennart.nilsson@gmail.com>
2014-09-17 16:57:09 +0200

    Added Google play services to project.

--- otto/app/src/main/java/truckerboys/otto/MainActivity.java
+++ otto/app/src/main/java/truckerboys/otto/MainActivity.java
@@ -1,7 +1,28 @@
 package truckerboys.otto;
+import com.google.android.gms.maps.*;
+import com.google.android.gms.maps.model.*;
+import android.app.Activity;
+import android.os.Bundle;
 
-/**
- * Created by Martin on 16/09/2014.
- */
-public class MainActivity {
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.map_layout);
+
+        // Get a handle to the Map Fragment
+        GoogleMap map = ((MapFragment) getFragmentManager()
+                .findFragmentById(R.id.map)).getMap();
+
+        LatLng sydney = new LatLng(-33.867, 151.206);
+
+        map.setMyLocationEnabled(true);
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney, 13));
+
+        map.addMarker(new MarkerOptions()
+                .title("Sydney")
+                .snippet("The most populous city in Australia.")
+                .position(sydney));
+    }
 }
