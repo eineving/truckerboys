550fe0a940f501123260b1f1c731745a562f8d51
Daniel Eineving <daniel@eineving.se>
2014-09-20 18:13:56 +0200

    Added more fields to Location

--- otto/app/src/main/java/truckerboys/otto/planner/TripPlanner.java
+++ otto/app/src/main/java/truckerboys/otto/planner/TripPlanner.java
@@ -1,28 +1,32 @@
 package truckerboys.otto.planner;
 
+import java.util.List;
+
+import truckerboys.otto.driver.Session;
+import truckerboys.otto.driver.User;
 import truckerboys.otto.mapAPI.IMap;
 import truckerboys.otto.planner.positions.Location;
 
 public class TripPlanner {
+    private User user;
     private IRegulationHandler regulationHandler;
     private IMap mapProvider;
 
     private Location finalDestination;
 
-    public TripPlanner(IRegulationHandler regulationHandler, IMap mapProvider) {
+    public TripPlanner(IRegulationHandler regulationHandler, IMap mapProvider, User user) {
         this.regulationHandler = regulationHandler;
         this.mapProvider = mapProvider;
+        this.user=user;
     }
 
-    public void addFinalDestination(Location finalDestination) {
+    public void calculateNewRouteTo(Location finalDestination){
+        addFinalDestination(finalDestination);
+        mapProvider.calculateRoute();
+    }
+
+    private void addFinalDestination(Location finalDestination) {
         this.finalDestination = finalDestination;
+        mapProvider.setFinalDestination(finalDestination);
     }
-
-    /**
-     * Calculates and sends the wanted route to the mapAPI
-     */
-    public void calculateNewRoute(){
-        //TODO Eineving insert code here
-    }
-
 }
--- otto/app/src/main/java/truckerboys/otto/planner/positions/Location.java
+++ otto/app/src/main/java/truckerboys/otto/planner/positions/Location.java
@@ -7,6 +7,13 @@ public abstract class Location {
     private double longitude;
     private double latitude;
 
+    //Address
+    private String country;
+    private String city;
+    private String zipCode;
+    private String street;
+    private String streetNumber;
+
     public Location(double longitude, double latitude) {
         this.longitude = longitude;
         this.latitude = latitude;
@@ -20,4 +27,23 @@ public abstract class Location {
         return latitude;
     }
 
+    public String getCountry() {
+        return country;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public String getZipCode() {
+        return zipCode;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public String getStreetNumber() {
+        return streetNumber;
+    }
 }
