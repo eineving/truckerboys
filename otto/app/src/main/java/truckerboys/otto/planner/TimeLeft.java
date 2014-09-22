bf898021b831eea89c02e770fbf53e0ba92eebaf
Andreas Pegelow <andpegelow@gmail.com>
2014-09-22 17:49:06 +0200

    Created the help class TimeLeft

---
+++ otto/app/src/main/java/truckerboys/otto/planner/TimeLeft.java
@@ -0,0 +1,33 @@
+package truckerboys.otto.planner;
+
+import org.joda.time.Duration;
+
+/**
+ * Created by Andreas Pegelow on 2014-09-22.
+ */
+public class TimeLeft {
+    Duration timeLeft, extendedTime;
+
+    public TimeLeft(Duration timeLeft, Duration supplementTime) {
+        this.timeLeft = timeLeft;
+        this.extendedTime = supplementTime;
+
+    }
+
+    /**
+     * Returns the remaining time in a normal scenario.
+     * @return The remaining time.
+     */
+    public Duration getTimeLeft() {
+        return timeLeft;
+    }
+
+    /**
+     * Returns the remaining time of the extended time.
+     * This is applied when regulations allow to extend a driving session.
+     * @return The remaining extended time.
+     */
+    public Duration getExtendedTime() {
+        return extendedTime;
+    }
+}
