dd8461d6079c00731b562b8039146fadc28d9f34
Simon Petersson <peterssonaren@gmail.com>
2014-09-23 12:11:14 +0200

    Added debug view.

---
+++ otto/app/src/main/java/truckerboys/otto/DebugView.java
@@ -0,0 +1,14 @@
+package truckerboys.otto;
+
+/**
+ * Created by Simon Petersson on 2014-09-23.
+ *
+ * Temporary View to display debug information, feel free to
+ * add any debug text that you feel is relevant. This will be removed when the final release
+ * is done.
+ */
+public class DebugView extends FragmentView {
+    public DebugView(){
+        super("Debug", R.layout.fragment_debug);
+    }
+}
