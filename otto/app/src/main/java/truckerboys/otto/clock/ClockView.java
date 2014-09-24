ec7cb2ea7de7cb98648a7197dfeeaecec8732116
siMs0n <simon.c.nielsen@gmail.com>
2014-09-24 15:04:35 +0200

    Added onCreateView and setTimeLeft method

--- otto/app/src/main/java/truckerboys/otto/clock/ClockView.java
+++ otto/app/src/main/java/truckerboys/otto/clock/ClockView.java
@@ -1,6 +1,12 @@
 package truckerboys.otto.clock;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.TextView;
+
+import org.joda.time.Duration;
 
 import truckerboys.otto.FragmentView;
 import truckerboys.otto.R;
@@ -9,20 +15,33 @@ import truckerboys.otto.R;
  * Created by Mikael Malmqvist on 2014-09-18.
  */
 public class ClockView extends FragmentView {
+    View rootView;
     TextView timeLeft, stopTL1, stopTL2, stopTL3, stopN1, stopN2, stopN3;
     public ClockView(){
         super("Clock", R.layout.fragment_clock);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        rootView  = inflater.inflate(R.layout.fragment_clock, container, false);
+
         initiateVariables();
+
+        return rootView;
     }
 
     private void initiateVariables(){
-        timeLeft = (TextView) getActivity().findViewById(R.id.clockText);
+        timeLeft = (TextView) rootView.findViewById(R.id.clockText);
         timeLeft.setText("04:22");
-        stopTL1 = (TextView) getActivity().findViewById(R.id.timeStop1);
-        stopTL2 = (TextView) getActivity().findViewById(R.id.timeStop2);
-        stopTL3 = (TextView) getActivity().findViewById(R.id.timeStop3);
-        stopN1 = (TextView) getActivity().findViewById(R.id.nameStop1);
-        stopN2 = (TextView) getActivity().findViewById(R.id.nameStop2);
-        stopN3 = (TextView) getActivity().findViewById(R.id.nameStop3);
+        stopTL1 = (TextView) rootView.findViewById(R.id.timeStop1);
+        stopTL2 = (TextView) rootView.findViewById(R.id.timeStop2);
+        stopTL3 = (TextView) rootView.findViewById(R.id.timeStop3);
+        stopN1 = (TextView) rootView.findViewById(R.id.nameStop1);
+        stopN2 = (TextView) rootView.findViewById(R.id.nameStop2);
+        stopN3 = (TextView) rootView.findViewById(R.id.nameStop3);
+    }
+
+    public void setTimeLeft(Duration timeLeft){
+        this.timeLeft.setText(timeLeft.toString());
     }
 }
