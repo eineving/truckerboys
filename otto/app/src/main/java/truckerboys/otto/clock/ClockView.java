bc58d446793ceb51307a504bc9cc1db6a7f28bab
siMs0n <simon.c.nielsen@gmail.com>
2014-09-25 09:02:25 +0200

    Added RestStop class and methods for setting labels in ClockView

--- otto/app/src/main/java/truckerboys/otto/clock/ClockView.java
+++ otto/app/src/main/java/truckerboys/otto/clock/ClockView.java
@@ -7,6 +7,12 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 import org.joda.time.Duration;
+import org.joda.time.Period;
+import org.joda.time.format.PeriodFormatter;
+import org.joda.time.format.PeriodFormatterBuilder;
+
+import java.util.ArrayDeque;
+import java.util.Iterator;
 
 import truckerboys.otto.FragmentView;
 import truckerboys.otto.R;
@@ -17,6 +23,8 @@ import truckerboys.otto.R;
 public class ClockView extends FragmentView {
     View rootView;
     TextView timeLeft, stopTL1, stopTL2, stopTL3, stopN1, stopN2, stopN3;
+    ArrayDeque<RestStop> stops = new ArrayDeque<RestStop>();
+
     public ClockView(){
         super("Clock", R.layout.fragment_clock);
     }
@@ -42,6 +50,54 @@ public class ClockView extends FragmentView {
     }
 
     public void setTimeLeft(Duration timeLeft){
-        this.timeLeft.setText(timeLeft.toString());
+        this.timeLeft.setText(getTimeAsFormatedString(timeLeft));
+    }
+
+    public void addNewRestStop(RestStop newRestStop){
+
+        stops.addFirst(newRestStop);
+        if(stops.size()>3){
+            stops.removeLast();
+        }
+
+        setLabels();
+
+    }
+
+    public void update(){
+        setLabels();
+    }
+
+    private void setLabels(){
+        Iterator it = stops.descendingIterator();
+        if(it.hasNext()) {
+            RestStop stop = (RestStop) it.next();
+            stopTL1.setText(getTimeAsFormatedString(stop.getTimeLeft()));
+            stopN1.setText(stop.getName());
+        }
+        if(it.hasNext()) {
+            RestStop stop = (RestStop) it.next();
+            stopTL2.setText(getTimeAsFormatedString(stop.getTimeLeft()));
+            stopN2.setText(stop.getName());
+        }
+        if(it.hasNext()) {
+            RestStop stop = (RestStop) it.next();
+            stopTL3.setText(getTimeAsFormatedString(stop.getTimeLeft()));
+            stopN3.setText(stop.getName());
+        }
+    }
+
+    private String getTimeAsFormatedString(Duration time){
+
+        Period period = time.toPeriod();
+        PeriodFormatter minutesAndSeconds = new PeriodFormatterBuilder()
+                .printZeroAlways()
+                .appendMinutes()
+                .appendSeparator(":")
+                .appendSeconds()
+                .toFormatter();
+        String result = minutesAndSeconds.print(period);
+
+        return result;
     }
 }
