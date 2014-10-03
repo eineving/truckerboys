2b318941b7c7159f313e82766fc637d17dd2a2bd
nilss0n <martin.lennart.nilsson@gmail.com>
2014-10-03 15:09:13 +0200

    Started working on the tripplanner.

--- otto/app/src/main/java/truckerboys/otto/planner/TripPlanner.java
+++ otto/app/src/main/java/truckerboys/otto/planner/TripPlanner.java
@@ -1,5 +1,10 @@
 package truckerboys.otto.planner;
 
+import org.joda.time.Duration;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import truckerboys.otto.directionsAPI.IDirections;
 import truckerboys.otto.directionsAPI.Route;
 import truckerboys.otto.driver.User;
@@ -20,16 +25,46 @@ public class TripPlanner {
 
     /**
      * Calculates a new route for when the driver is not driving
-     *
-     * @param finalDestination target location to drive to
-     * @required the truck needs to be standing still
      */
     public Route calculateRoute(Location startLocation, Location endLocation) {
         try {
-            return directionsProvider.getRoute(startLocation.getLatLng(), endLocation);
+            Route route = directionsProvider.getRoute(startLocation.getLatLng(), endLocation);
+
+
+
+
+
+            return route;
         } catch (Exception e) {
             e.printStackTrace();
         }
         return null;
     }
+
+    /**
+     * Calculates the optimal times to take a break depending on the ETA to the destination
+     * and the driving regulations.
+     * @param ETA The ETA to the final destination.
+     * @return
+     */
+    private List<Duration> getOptimalBreaks(Duration ETA){
+        ArrayList<Duration> breaks = new ArrayList<Duration>();
+
+        Duration legTL = regulationHandler.getThisSessionTL(user.getHistory()).getTimeLeft();
+        Duration totalTL = ETA;
+
+        while(ETA.isLongerThan(legTL)){
+            breaks.add(legTL.minus(Duration.standardMinutes(10)));
+            totalTL = totalTL.minus(legTL);
+            // How do we predict the allowed time for future sessions
+            //The code below will not work as intended.
+            legTL = Duration.standardHours(4).plus(Duration.standardMinutes(30));
+        }
+
+        //Should not only return times of the breaks but also the duration of the breaks.
+        return breaks;
+    }
+
+
+
 }
