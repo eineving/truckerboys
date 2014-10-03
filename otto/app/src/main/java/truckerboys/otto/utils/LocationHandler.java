3471aad110568ec076b3116169cd0cb61d85849e
Simon Petersson <peterssonaren@gmail.com>
2014-10-03 15:20:17 +0200

    Added a LocationHandler that holds our current location and fires an event upon location changed.

---
+++ otto/app/src/main/java/truckerboys/otto/utils/LocationHandler.java
@@ -0,0 +1,89 @@
+package truckerboys.otto.utils;
+
+import android.content.Context;
+import android.location.Location;
+import android.os.Bundle;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesClient;
+import com.google.android.gms.location.LocationClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+
+import truckerboys.otto.utils.eventhandler.EventTruck;
+import truckerboys.otto.utils.eventhandler.events.LocationChangedEvent;
+
+/**
+ * Created by Simon Petersson on 2014-10-03.
+ *
+ * Class that holds the device current position and fires an LocationChangedEvent when
+ * Googles Location API fires that our location has changed.
+ */
+public class LocationHandler implements GooglePlayServicesClient.OnConnectionFailedListener,
+        GooglePlayServicesClient.ConnectionCallbacks,
+        LocationListener {
+
+    private static final int LOCATION_REQUEST_INTERVAL_MS = 500;
+
+    private LocationClient locationClient;
+
+    private static Location currentLocation;
+    private static boolean connected = false;
+
+    public LocationHandler(Context context){
+        locationClient = new LocationClient(context, this, this);
+        locationClient.connect();
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+        //Setup the location request with intervals and accuracy.
+        LocationRequest locationRequest = new LocationRequest();
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        locationRequest.setInterval(LOCATION_REQUEST_INTERVAL_MS);
+
+        //Request new location updates to this LocationListener.
+        locationClient.requestLocationUpdates(locationRequest, this);
+
+        //Define that the LocationHandler is connected to the GPS.
+        connected = true;
+    }
+
+    @Override
+    public void onDisconnected() {
+        connected = false;
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        if(isMoreAccurate(location)) {
+            EventTruck.getInstance().newEvent(new LocationChangedEvent(location, getCurrentLocation()));
+            this.currentLocation = new Location(location);
+        }
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+
+    }
+
+    /**
+     * Checks if a location is more accurate and trushworthy then the last one that was set in
+     * the LocationHandler. Firing this when in onLocationChanged will make sure the current
+     * saved position is always the most accurate.
+     * @param newLocation The location to check against the last one set.
+     * @return True if the new location is more accurate.
+     */
+    private boolean isMoreAccurate(Location newLocation) {
+        //TODO Implement method to check if the new location is more accurate than the last one that was set.
+        return true;
+    }
+
+    public static boolean isConnected() {
+        return connected;
+    }
+
+    public Location getCurrentLocation() {
+        return currentLocation;
+    }
+}
