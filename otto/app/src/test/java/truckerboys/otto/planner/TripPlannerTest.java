cb05c959c9b16e55e36581ec869b9e06746b0a4b
Daniel Eineving <daniel@eineving.se>
2014-10-15 15:29:26 +0200

    Started to work on junit test for TripPlanner

---
+++ otto/app/src/test/java/truckerboys/otto/planner/TripPlannerTest.java
@@ -0,0 +1,27 @@
+package truckerboys.otto.planner;
+
+import junit.framework.TestCase;
+
+public class TripPlannerTest extends TestCase {
+
+    public void setUp() throws Exception {
+        super.setUp();
+
+    }
+
+    public void testUpdateRoute() throws Exception {
+
+    }
+
+    public void testGetRoute() throws Exception {
+
+    }
+
+    public void testSetChoosenStop() throws Exception {
+
+    }
+
+    public void testSetNewRoute() throws Exception {
+
+    }
+}
