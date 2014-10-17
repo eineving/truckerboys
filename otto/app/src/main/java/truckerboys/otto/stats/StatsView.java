2cffd6fc348ea2443e65caedbecafdbb15d15261
Mikael Malmqvist <mikael.malmqvist.web@gmail.com>
2014-10-17 11:27:07 +0200

    Reworked how we load/save stats and cleaned up a bunch of code

--- otto/app/src/main/java/truckerboys/otto/stats/StatsView.java
+++ otto/app/src/main/java/truckerboys/otto/stats/StatsView.java
@@ -1,12 +1,9 @@
 package truckerboys.otto.stats;
 
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
 import android.support.v4.app.Fragment;
-import android.swedspot.automotiveapi.AutomotiveSignal;
-import android.swedspot.scs.data.SCSFloat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,26 +12,15 @@ import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
 
-import org.joda.time.Duration;
-import org.joda.time.Instant;
-import org.w3c.dom.Text;
-
-import java.util.ArrayList;
-import java.util.List;
 
 import truckerboys.otto.R;
-import truckerboys.otto.driver.User;
 import truckerboys.otto.utils.eventhandler.EventTruck;
 import truckerboys.otto.utils.eventhandler.IEventListener;
 import truckerboys.otto.utils.eventhandler.events.DistanceByFuelEvent;
 import truckerboys.otto.utils.eventhandler.events.Event;
 import truckerboys.otto.utils.eventhandler.events.RestorePreferencesEvent;
-import truckerboys.otto.utils.eventhandler.events.SettingsChangedEvent;
-import truckerboys.otto.utils.eventhandler.events.TimeDrivenEvent;
+import truckerboys.otto.utils.eventhandler.events.StatsViewStoppedEvent;
 import truckerboys.otto.utils.eventhandler.events.TotalDistanceEvent;
-import truckerboys.otto.vehicle.IVehicleListener;
-import truckerboys.otto.vehicle.VehicleInterface;
-import truckerboys.otto.vehicle.VehicleSignalID;
 import truckerboys.otto.IView;
 
 
@@ -47,8 +33,6 @@ import truckerboys.otto.IView;
 public class StatsView extends Fragment implements IView, IEventListener{
 
     private View rootView;
-    private static final String SETTINGS = "Settings_file";
-    private static final String STATS = "Stats_file";
 
     // Todays stats
     private TextView timeToday;
@@ -66,50 +50,13 @@ public class StatsView extends Fragment implements IView, IEventListener{
     ArrayAdapter<String> sessionAdapter;
     private ListView historyList;
 
-    private String distanceUnit = "";
-    private String fuelUnit = "";
-
-    //region Runnables
+    // Updatehandler
     private Handler updateHandler = new Handler(Looper.getMainLooper());
-    private Runnable statsUpdater = new Runnable() {
-        public void run() {
-            updateStats();
-            updateHandler.postDelayed(statsUpdater, 600000); // Updates each 5sec
-        }
-    };
+
 
     public StatsView(){
     }
 
-
-    /**
-     * Method for updating the stats.
-     * This method is run each 5min
-     */
-    public void updateStats() {
-        Duration durationToday;
-        Duration durationTotal;
-
-
-        if(User.getInstance().getHistory() != null) {
-
-            // Time driven today
-            durationToday = User.getInstance().getHistory().getActiveTimeSinceLastDailyBreak();
-
-            // Total time driven since start
-            durationTotal = User.getInstance().getHistory().getActiveTimeSince(new Instant(0));
-
-            double timeDrivenToday = durationToday.getStandardMinutes()/60;
-            double timeDrivenTotal = durationTotal.getStandardMinutes()/60;
-
-            timeToday.setText(timeDrivenToday + " h");
-            timeTotal.setText(timeDrivenTotal + " h");
-
-
-            EventTruck.getInstance().newEvent(new TimeDrivenEvent(timeDrivenToday, timeDrivenTotal));
-        }
-
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -138,30 +85,19 @@ public class StatsView extends Fragment implements IView, IEventListener{
         // Restores preferences for settings in presenter
         EventTruck.getInstance().newEvent(new RestorePreferencesEvent());
 
-        statsUpdater.run();
-
         return rootView;
     }
 
     /**
-     * On stop, save the settings displayed in the view.
+     * On stop, notify the presenter through an event.
+     * The presenter will furthermore save the data from the model.
      */
     @Override
     public void onStop() {
         super.onStop();
 
-        //TODO Put this code in the presenter and get values from model
-
-        SharedPreferences.Editor editor = getActivity().getSharedPreferences(STATS, 0).edit();
-
-        editor.putFloat("timeToday", Float.parseFloat(timeToday.getText().toString().substring(0, timeToday.getText().toString().length() - 2)));
-        editor.putFloat("timeTotal", Float.parseFloat(timeTotal.getText().toString().substring(0, timeTotal.getText().toString().length() - 2)));
-        editor.putFloat("distanceTotal", Float.parseFloat(distanceTotal.getText().toString().substring(0, distanceTotal.getText().toString().length() - 3)));
-        editor.putFloat("fuelTotal", Float.parseFloat(fuelTotal.getText().toString().substring(0, fuelTotal.getText().toString().length() - 2)));
-        editor.putFloat("distanceByFuel", Float.parseFloat(distanceByFuel.getText().toString().substring(0, distanceByFuel.getText().toString().length() - 5)));
-
-
-        editor.commit();
+        // Notifies the presenter that the view has stopped
+        EventTruck.getInstance().newEvent(new StatsViewStoppedEvent());
 
     }
 
@@ -177,23 +113,6 @@ public class StatsView extends Fragment implements IView, IEventListener{
 
         historyList.setLayoutParams(new LinearLayout.LayoutParams(
                 1000, historyList.getAdapter().getCount()*125));
-
-    }
-
-    /**
-     * Method for setting a new unit system.
-     * This method is to be called from the presenter.
-     * @param system metric/imperial
-     */
-    public void updateUnits(String system) {
-        if(system.equals("imperial")) {
-            fuelUnit = ""; // MILES
-            distanceUnit = ""; // GALLONS
-
-        } else {
-            fuelUnit = "";
-            distanceUnit = "";
-        }
 
     }
 
@@ -255,17 +174,6 @@ public class StatsView extends Fragment implements IView, IEventListener{
 
     @Override
     public void performEvent(final Event event) {
-        if(event.isType(SettingsChangedEvent.class)) {
-
-            // read from file and set String called system based on that
-            SharedPreferences.Editor editor = getActivity().getSharedPreferences(SETTINGS, 0).edit();
-
-            editor.putString("system", ((SettingsChangedEvent) event).getSystem());
-
-            editor.commit();
-
-        }
-
 
         if(event.isType(TotalDistanceEvent.class)) {
             // Update view if new total distance signal is sent
